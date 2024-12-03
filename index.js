844a3ec4a8d83ae6ba413399925164fa06afe6a5
Brandon Dalton <dabigin@gmail.com>
2024-12-03 03:37:06 -0600

    Section 40:Middleware: The Key To Express Lecture 433 More Middleware Practice

--- index.js
+++ index.js
@@ -2,33 +2,41 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+app.use(morgan('tiny'))
 
-
-app.use(morgan('common'))
 app.use((req, res, next) => {
-    console.log("THIS IS MY FIRST MIDDLEWARE!!!")
-    return next();
-    console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+    req.requestTime = Date.now()
+    console.log(req.method, req.path);
+    next();
 })
 
-app.use((req, rest, next) => {
-    console.log("THIS IS MY SECOND MIDDLEWARE!!!")
-    return next()
-})
 
-app.use((req, rest, next) => {
-    console.log("THIS IS MY THIRD MIDDLEWARE!!!")
-    return next()
-})
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLEWARE!!!")
+//     return next();
+//     console.log("THIS IS MY FIRST MIDDLEWARE - AFTER CALLING NEXT()")
+// })
+
+// app.use((req, rest, next) => {
+//     console.log("THIS IS MY SECOND MIDDLEWARE!!!")
+//     return next()
+// })
+
+// app.use((req, rest, next) => {
+//     console.log("THIS IS MY THIRD MIDDLEWARE!!!")
+//     return next()
+// })
 
 app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('HOME PAGE!')
 })
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('WOOF WOOF!')
 })
-
 
 app.listen(3000, () => {
     console.log('App is running on localhost:3000')
