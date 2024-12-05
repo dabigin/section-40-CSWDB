5929b1a67e9cb57c78ceeef7f16e2ef52359d6ec
Brandon Dalton <dabigin@gmail.com>
2024-12-05 06:20:13 -0600

    Section 40: Middleware:The Key To Express Lecture 436.Protecting Specific Routes - modified index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/dogs', (req, res, next) => {
     next();
 })
 
-app.use((req, res, next) => {
+const verifyPassword = ((req, res, next) => {
     const { password } = req.query;
     if (password === 'chickennugget') {
         return next();
@@ -51,7 +51,7 @@ app.get('/dogs', (req, res) => {
     res.send('WOOF WOOF!')
 })
 
-app.get('/secret', (req, res, next) => {
+app.get('/secret', verifyPassword, (req, res, next) => {
     res.send("MY SECRET IS: Sometimes I wear headphones in public so I don't have to talk to anyone.")
 })
 
